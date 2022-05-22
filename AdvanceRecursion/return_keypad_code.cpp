2269bd2db1e83f1868e0d56db6ea5badde5bc8a2
Harsh Rishiwal <90250498+harshRishi@users.noreply.github.com>
2022-05-22 09:30:47 +0530

    Create return_keypad_code.cpp

---
+++ AdvanceRecursion/return_keypad_code.cpp
@@ -0,0 +1,81 @@
+
+/*
+Return Keypad Code
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+The numbers and their corresponding codes are given below:
+0: ""
+1: ""
+2: "abc"
+3: "def"
+4: "ghi"
+5: "jkl"
+6: "mno"
+7: "pqrs"
+8: "tuv"
+9: "wxyz"
+Return empty string for numbers 0 and 1.
+Note:
+1. The order of strings are not important.
+2. The input number will have digits between: [2, 9].
+Input Format :
+First line of input will contain T number of test cases.
+Each input consists of a single line containing an integer n.
+Output Format :
+For each test case, print all possible strings in different lines.
+Constraints :
+1 <= T <= 100
+1 <= n <= 10^6
+Sample Input:
+1
+23
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string> keypad(int n, vector<string> keyChar){
+    if(n<=1){
+        vector<string> output;
+        output.push_back("");
+        return output;
+    }
+    int lastDigit=n%10;
+    int smallNum=n/10;
+    vector<string> smalloutput;
+    vector<string> output;
+
+    smalloutput=keypad(smallNum, keyChar);
+    string letters=keyChar[lastDigit];
+   
+    for(int i=0;i<letters.length();i++){
+        for(int j=0;j<smalloutput.size();j++){
+            output.push_back(smalloutput[j]+letters[i]);
+        }
+    }
+    return output;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> keyChar = {"", "","abc" ,"def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> output = keypad(n, keyChar);
+        for(int i=0;i<output.size();i++)
+            cout<<output[i]<<'\n';
+    }
+    // write your code here
+    return 0;
+}
